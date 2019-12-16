51ba7b821db6d29edf0fb34e005ec678a05c88dc
Ricardo Souza <souzaariick@gmail.com>
2019-12-16 19:59:12 -0300

    Splash Screen Aprimorada

--- app/src/main/java/com/example/ultimateaccountmanager/MainActivity.kt
+++ app/src/main/java/com/example/ultimateaccountmanager/MainActivity.kt
@@ -10,11 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Glide
-            .with(this)
-            .load("https://outfits.ferobraglobal.com/animoutfit.php?id=130&addons=3&head=123&body=12&legs=23&feet=31&mount=92&direction=3")
-            .into(player_image)
     }
 
 
--- app/src/main/java/com/example/ultimateaccountmanager/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/example/ultimateaccountmanager/splash/SplashScreenActivity.kt
@@ -5,43 +5,62 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.util.DisplayMetrics
+import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationSet
+import android.view.animation.DecelerateInterpolator
 import android.view.animation.LinearInterpolator
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.ultimateaccountmanager.MainActivity
 import com.example.ultimateaccountmanager.R
 import kotlinx.android.synthetic.main.activity_splash_screen.*
+import kotlin.random.Random
 
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    private val TIMEOUT_SPLASH_SCREEN: Long = 9000 //3 Segundos
+    private val timeoutSplashScreen: Long = 6000 //3 Segundos
+    private val random = Random.nextInt(1, 100)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Glide
-            .with(this)
-            .load("https://outfits.ferobraglobal.com/animoutfit.php?id=130&addons=3&head=123&body=12&legs=23&feet=31&mount=92&direction=3")
-            .into(spl_logo_img)
 
+        spl_logo_name.alpha = 0f
         generateSplashAnimation()
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
-        }, TIMEOUT_SPLASH_SCREEN)
+        }, timeoutSplashScreen)
+    }
+
+    fun fadeInGenarate(duration: Long): AlphaAnimation {
+        val fadeIn = AlphaAnimation(0f, 1f)
+        fadeIn.interpolator = DecelerateInterpolator()
+        fadeIn.duration = duration
+
+        return fadeIn
     }
 
     fun generateSplashAnimation() {
-        splashLogoAnimation(TIMEOUT_SPLASH_SCREEN / 2)
-//        splashAnimateBackground()
+        splashLogoAnimation(timeoutSplashScreen / 2)
+        splashAnimateBackground()
     }
 
 
-    fun splashAnimateBackground(repeat: Int = 9, timeout: Long = TIMEOUT_SPLASH_SCREEN) {
+    fun generateImageSplash(random: Int, direction: Int = 3, imageView: ImageView = spl_logo_img) {
+        Glide
+            .with(this)
+            .load("https://outfits.ferobraglobal.com/animoutfit.php?id=130&addons=3&head=123&body=12&legs=23&feet=31&mount=${random}&direction=${direction}")
+            .into(imageView)
+    }
+
+
+    fun splashAnimateBackground(repeat: Int = 1, timeout: Long = timeoutSplashScreen) {
 
         val objectAnimator = ObjectAnimator.ofObject(
             layout_splash,
@@ -59,7 +78,6 @@ class SplashScreenActivity : AppCompatActivity() {
                 this,
                 R.color.colorPrimaryDark
             )
-//            ContextCompat.getDrawable(this, R.drawable.teste)
         )
 
         objectAnimator.repeatCount = repeat
@@ -70,25 +88,29 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     fun splashLogoAnimation(timer: Long, reverse: Boolean = false) {
+
+        val animation = AnimationSet(false)
+        animation.addAnimation(fadeInGenarate(2000))
+        spl_logo_img.animation = animation
+
         val displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
         val screenHeight = displayMetrics.heightPixels
+//        val screenWith = displayMetrics.widthPixels
 
-        val valueAnimator = ValueAnimator.ofFloat(-500f, screenHeight.toFloat())
+        val valueAnimator = ValueAnimator.ofFloat(-screenHeight.toFloat(), screenHeight.toFloat())
+//        val valueAnimator = ValueAnimator.ofFloat(-screenWith.toFloat(), screenWith.toFloat())
         valueAnimator.addUpdateListener {
             val value = it.animatedValue as Float
             spl_logo_img.translationY = value
+//            spl_logo_img.translationX = value
         }
 
         valueAnimator.interpolator = LinearInterpolator()
         valueAnimator.duration = (timer - 100)
 
         if (reverse) {
-            Glide
-                .with(this)
-                .load("https://outfits.ferobraglobal.com/animoutfit.php?id=130&addons=3&head=123&body=12&legs=23&feet=31&mount=92&direction=1")
-                .into(spl_logo_img)
-//            spl_logo_img.rotationX = 180f
+            generateImageSplash(random, 4)
             valueAnimator.reverse()
         } else {
             valueAnimator.addListener(object : Animator.AnimatorListener {
@@ -96,7 +118,12 @@ class SplashScreenActivity : AppCompatActivity() {
                 }
 
                 override fun onAnimationEnd(p0: Animator?) {
-                    splashLogoAnimation(TIMEOUT_SPLASH_SCREEN / 2, true)
+                    //TODO ADD ANIMATION LOGO
+                    val animateLogo = AnimationSet(false)
+                    animateLogo.addAnimation(fadeInGenarate(1000))
+                    spl_logo_name.alpha = 1f
+                    spl_logo_name.animation = animateLogo
+
                 }
 
                 override fun onAnimationCancel(p0: Animator?) {
@@ -105,6 +132,7 @@ class SplashScreenActivity : AppCompatActivity() {
                 override fun onAnimationStart(p0: Animator?) {
                 }
             })
+            generateImageSplash(random)
             valueAnimator.start()
         }
     }
