afa29f08c656a5407bf93687df81facec5111d0b
Ricardo Souza <souzaariick@gmail.com>
2019-12-23 02:30:54 -0300

    removed some trash on code

--- app/src/main/java/com/example/ultimateaccountmanager/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/ultimateaccountmanager/ui/login/LoginFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.ultimateaccountmanager.R
@@ -40,7 +39,6 @@ class LoginFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
         Timber.plant(Timber.DebugTree())
         edt_login_username.requestFocus()
-        // TODO: Use the ViewModel
     }
 
     override fun onResume() {
